f311eb0b03d4b7d4d80895bd85c242b82e74e4fc
ifatinha <ifatinha99@gmail.com>
2025-06-27 11:30:03 -0300

    js

---
+++ intermediate-projects/timer-alarm/js/index.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const display = document.querySelector("#display");
+  const minutes = document.querySelector("#minutes");
+  const seconds = document.querySelector("#seconds");
+  const startBtn = document.querySelector("#start");
+  const pauseBtn = document.querySelector("#pause");
+  const resetBtn = document.querySelector("#reset");
+  const alarmSound = document.querySelector("alarm-sound");
+  let timer;
+  
+});
