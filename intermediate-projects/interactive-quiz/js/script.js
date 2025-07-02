90de08574d676c41831c2988149e212765ac6006
ifatinha <ifatinha99@gmail.com>
2025-07-02 12:58:31 -0300

    Inicia script do quiz interativo com seleção de elementos do DOM

--- intermediate-projects/interactive-quiz/js/script.js
+++ intermediate-projects/interactive-quiz/js/script.js
@@ -1,1 +1,9 @@
 import { quizData } from "./questions.js";
+document.addEventListener("DOMContentLoaded", () => {
+  // Seleciona os elementos do DOM
+  const questionEl = document.querySelector("#question");
+  const nextButton = document.querySelector("#next-btn");
+  const scoreBox = document.querySelector("#score-box");
+  const scoreEl = document.querySelector("#score");
+  const restartButton = document.querySelector("#restart-btn");
+});
